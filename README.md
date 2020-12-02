f9445790bb27e477807eabd8565edce81b916a9c
zhangxujun <zxj123036>
2020-12-02 13:44:50 +0800

    优化命令

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # koa-cli
 
-create a koa project
+## create a koa project
+
+```
+npm i -g @z-xj/koa-cli
+koa-cli init
+```
---
+++ bin/koa-cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const Creator = require('../src/creator.js')
+const package = require('../package-lock.json')
+const program = require('commander');
+
+program.version(package.version, '-v, --version')
+program
+    .command('init')
+    .action(() => {
+        const project = new Creator()
+        project.init()
+    })
+program.parse(process.argv);
+
